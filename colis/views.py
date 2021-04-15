d10945510c57f56882d97db6982678fd1a321c2d
Denis <dineresc@gmail.com>
2021-04-15 16:23:40 -0300

    creating search for colis

--- colis/views.py
+++ colis/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
-# Create your views here.
+from colis.models import Coli
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>hello world </h1>")
+
+def coli_detail_view(request, coli_id, *args, **kwargs):
+    try:
+        coli = Coli.objects.get(id=coli_id)
+    except:
+        raise Http404
+    
+    return HttpResponse(f"<h1>hello {coli_id} </h1>")
