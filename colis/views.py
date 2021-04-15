31bc268289184de4b6f87edaf7f15aa7b3bc3cd4
Denis <dineresc@gmail.com>
2021-04-15 16:40:16 -0300

    more updates to json response

--- colis/views.py
+++ colis/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from colis.models import Coli
@@ -8,9 +8,19 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>hello world </h1>")
 
 def coli_detail_view(request, coli_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JS or Java
+    """
+    status = 200
+    data = {
+        "id": coli_id,
+    }
     try:
         coli = Coli.objects.get(id=coli_id)
+        data['content'] = coli.content
     except:
-        raise Http404
-    
-    return HttpResponse(f"<h1>hello {coli_id} </h1>")
+        data['content'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
